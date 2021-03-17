492b336d301ab0c3dfb46db9be2f4cc95cfbea32
DawsonJ7355608 <72110797+DawsonJ7355608@users.noreply.github.com>
2021-03-16 19:54:27 -0700

    Update Faculty.java

--- src/Faculty.java
+++ src/Faculty.java
@@ -3,6 +3,24 @@ public abstract class Faculty extends User {
 
 	protected String typeOfFaculty; //typeOfFaculty refers to specifically what department the faculty member belongs to.
 	
+	/* Getters and Setters */
+	public String getTypeOfFaculty() {
+		return typeOfFaculty;
+	}
+
+	public void setTypeOfFaculty(String typeOfFaculty) {
+		this.typeOfFaculty = typeOfFaculty;
+	}
+
+	/* constructors */
+	public Faculty() {}
+	public Faculty(String name, String username, String password, boolean 
+			ableToEditAnyCourse, boolean ableToEditMyCourse, boolean ableToEnrollStudents, 
+			boolean ableToEnrollInClasses, String typeOfFaculty) {
+		super(name, username, password, ableToEditAnyCourse, ableToEditMyCourse, ableToEnrollStudents, ableToEnrollInClasses);
+		this.typeOfFaculty = typeOfFaculty;
+	}
+	
 	@Override
 	public boolean login() {
 		// TODO Auto-generated method stub
