cf983b2074669b267f6a1325085797018f10bdd3
DawsonJ7355608 <72110797+DawsonJ7355608@users.noreply.github.com>
2021-03-29 16:21:02 -0700

    Create SearchForm.java
    SearchForm.java is the form called when searching for an course on the main forms for Administrator, Professor, or Student.

---
+++ src/SearchForm.java
@@ -0,0 +1,141 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTable;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import javax.swing.table.DefaultTableModel;
+
+public class SearchForm extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTable tblSearchResults;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			SearchForm dialog = new SearchForm();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public SearchForm() {
+		setTitle("Search results");
+		setBounds(100, 100, 700, 850);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{0, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0};
+		gbl_contentPanel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			tblSearchResults = new JTable();
+			tblSearchResults.setModel(new DefaultTableModel(
+				new Object[][] {
+					{"Subject", "Course No.", "Name", "Dates", "Days/Time", "Cr/Hr", "Instructors"},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null},
+				},
+				new String[] {
+					"New column", "New column", "New column", "New column", "New column", "New column", "New column"
+				}
+			));
+			tblSearchResults.getColumnModel().getColumn(0).setPreferredWidth(43);
+			tblSearchResults.getColumnModel().getColumn(1).setPreferredWidth(58);
+			tblSearchResults.getColumnModel().getColumn(2).setPreferredWidth(125);
+			tblSearchResults.getColumnModel().getColumn(5).setPreferredWidth(35);
+			tblSearchResults.getColumnModel().getColumn(6).setPreferredWidth(100);
+			tblSearchResults.setFont(new Font("Tahoma", Font.PLAIN, 14));
+			GridBagConstraints gbc_tblSearchResults = new GridBagConstraints();
+			gbc_tblSearchResults.fill = GridBagConstraints.BOTH;
+			gbc_tblSearchResults.gridx = 0;
+			gbc_tblSearchResults.gridy = 0;
+			contentPanel.add(tblSearchResults, gbc_tblSearchResults);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnAddCourse = new JButton("Add Course");
+				btnAddCourse.setFont(new Font("Tahoma", Font.PLAIN, 14));
+				buttonPane.add(btnAddCourse);
+			}
+			{
+				JButton btnOk = new JButton("OK");
+				btnOk.setFont(new Font("Tahoma", Font.PLAIN, 14));
+				btnOk.setActionCommand("OK");
+				buttonPane.add(btnOk);
+				getRootPane().setDefaultButton(btnOk);
+			}
+			{
+				JButton btnCancel = new JButton("Cancel");
+				btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+				btnCancel.setActionCommand("Cancel");
+				buttonPane.add(btnCancel);
+			}
+		}
+	}
+
+}
